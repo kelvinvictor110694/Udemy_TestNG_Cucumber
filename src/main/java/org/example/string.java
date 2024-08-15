25d6f804b95a97f5ade6457908e1c6386eda467a
Kelvin Victor <kelvin.victor007@gmail.com>
2024-08-15 21:25:11 +0530

    String Reversal except 2nd last character

--- src/main/java/org/example/string.java
+++ src/main/java/org/example/string.java
@@ -3,13 +3,29 @@ package org.example;
 public class string {
 
     public static void main(String[] args) {
+        printStringvalue();
+        reverseStringexceptsecondchar();
+    }
+
+    public static void printStringvalue() {
         String s = "kelvin victor";
         String[] splitvalue = s.split(" ");
-        for (int i=0;i<splitvalue.length;i++){
+        for (int i = 0; i < splitvalue.length; i++) {
             System.out.println(splitvalue[i]);
         }
-        for (int i=s.length()-1;i>=0;i--){
+        for (int i = s.length() - 1; i >= 0; i--) {
             System.out.println(s.charAt(i));
         }
+    }
+
+    public static void reverseStringexceptsecondchar(){
+        String s = "Independence";
+        String reverse="";
+        reverse = reverse+s.charAt(s.length()-1);
+        for (int i=s.length()-3; i>=1 ;i--){
+            reverse = reverse + s.charAt(i);
+        }
+        reverse = reverse+s.charAt(s.length()-2)+s.charAt(0);
+        System.out.println(reverse);
     }
 }
